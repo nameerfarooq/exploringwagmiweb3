646862f50dd5b27e9002cb854cb1a9e09d10021b
Nameerfarooq <nameerfarooquit@gmail.com>
2024-07-09 13:48:37 +0500

    connect wallet button added

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { WagmiProvider } from "wagmi";
-import { config } from "./config";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Profile } from "./profile";
+import { mainnet, polygon, optimism, arbitrum, base ,sepolia} from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
+const config = getDefaultConfig({
+  appName: "My RainbowKit App",
+  projectId: "74d426967c8ce41e051f27e4592eed5a",
+  chains: [mainnet, polygon, optimism, arbitrum, base,sepolia],
+});
+const queryClient = new QueryClient();
 const App = () => {
-  const queryClient = new QueryClient();
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <Profile />
+        <RainbowKitProvider>
+          <ConnectButton />
+        </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
   );
 };
-
 export default App;
